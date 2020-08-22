c2b0fb1d7477aa9e6cd556562ded0094a9e9a013
mushchlo <derpyop29@gmail.com>
2020-08-22 12:56:52 -0500

    commit

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"", "ln=$(date +%d | sed -r 's/.*(.)/\\1/'); [[ $ln = 1 ]] && suf=st ; [[ $ln = 2 ]] && suf=nd ; [[ $ln = 3 ]] && suf=rd; [[ -z $suf ]] && suf=th; date \"+%a, %b %-d$suf  %-I:%M;\"",	2,	0},
+	{" ",  "iwctl station wlan0 show | grep Connected | awk -F ' ' '{ print $3 }'",        60,     9},
+	{"", "battery",    30,     10},
+        {"", "getvolume",       0,      11},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
-static unsigned int delimLen = 5;
+static char delim[] = "|";
+static unsigned int delimLen = 3;
